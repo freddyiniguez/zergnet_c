b52490c49d196bd0cba652db2f0115adfac98b9f
Freddy Iniguez Lopez <iniguez.devt@gmail.com>
2016-11-29 17:03:12 -0600

    Adding files to the RSA assessment.

---
+++ zergnet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "zergnet.h"
+ 
+ int encryption_with_public_key(void){
+	printf("Hello, encryption!");
+    return 20;
+}
+
+
+int decryption_with_private_key(void){
+    printf("Hello, decryption!");
+    return 20;
+}
---
+++ zergnet.h
@@ -0,0 +1,7 @@
+#ifndef ZERGNET_H_
+#define ZERGNET_H_
+
+int encryption_with_public_key(void);
+int decryption_with_private_key(void);
+
+#endif // ZERGNET_H_
